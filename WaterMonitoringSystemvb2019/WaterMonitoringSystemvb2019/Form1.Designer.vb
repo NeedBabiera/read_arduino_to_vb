aef94f25d68274532237fb5f0ee2c1eb96d1054a
Nydyy <nydyybabierasocial@gmail.com>
2022-04-02 16:37:52 +0800

    Add files via upload
    converted into vb 2019

---
+++ WaterMonitoringSystemvb2019/WaterMonitoringSystemvb2019/Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.txtData = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'txtData
+        '
+        Me.txtData.Location = New System.Drawing.Point(218, 46)
+        Me.txtData.Multiline = True
+        Me.txtData.Name = "txtData"
+        Me.txtData.Size = New System.Drawing.Size(252, 117)
+        Me.txtData.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(218, 169)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(252, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "START"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtData)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtData As Windows.Forms.TextBox
+    Friend WithEvents Button1 As Windows.Forms.Button
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Timer1 As Windows.Forms.Timer
+End Class
---
+++ WaterMonitoringSystemvb2019/WaterMonitoringSystemvb2019/Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+    Dim myPort As String = ""
+    Dim readData As String = ""
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        PortSetup()
+
+    End Sub
+    Sub PortSetup()
+        Dim portAvailable As String() = System.IO.Ports.SerialPort.GetPortNames
+        Dim portConnected As String
+        For Each portConnected In portAvailable
+            myPort = portConnected
+        Next
+        Try
+            With SerialPort1
+                .PortName = myPort
+                .BaudRate = 9600
+                .Parity = IO.Ports.Parity.None
+                .DataBits = 8
+                .StopBits = IO.Ports.StopBits.One
+                .Handshake = IO.Ports.Handshake.None
+                .RtsEnable = True
+                .ReceivedBytesThreshold = 1
+                .NewLine = vbCr
+                .ReadTimeout = 1000
+                .Open()
+            End With
+
+            If SerialPort1.IsOpen Then
+                Me.Text = "Device Connected on: " & myPort
+            End If
+        Catch ex As Exception
+            Me.Text = "No Device Found"
+            MsgBox(ex.ToString)
+        End Try
+
+    End Sub
+
+    Sub Monitoring()
+        readData = SerialPort1.ReadExisting()
+
+        txtData.Text = readData
+
+
+    End Sub
+
+
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Monitoring()
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Timer1.Start()
+
+    End Sub
+End Class
